ee6d509bfabb7af8a6d5b81cdafc581ea7b3c3b4
soroosh shams <soroosh2468@hotmail.com>
2020-01-13 14:57:22 +1100

    icon and everything (Dropdown not working rails 6)

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,8 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("jquery")
+//= require semantic-ui
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
@@ -15,3 +17,6 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+$(document).on('turbolinks:load', function() {
+  $('.ui.dropdown').dropdown();
+})
