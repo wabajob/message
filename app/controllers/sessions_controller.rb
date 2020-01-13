51a8ce5ee7fdd0ddd2ebe56492bf9b98a9f3d96d
soroosh shams <soroosh2468@hotmail.com>
2020-01-13 16:40:57 +1100

    restrictions routes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,5 @@
 class SessionsController < ApplicationController
+  before_action :logged_in_redirect, only: [:new, :create]
 
 def new
 end
@@ -20,6 +21,12 @@ def create
     session[:user_id] = nil
     flash[:success] = "You have successfully logged out"
     redirect_to login_path
+  end
+
+  def private
+    if logged_in?
+      flash[:error]= "You are already logged in"
+      redirect_to root_path
   end
 
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -19,4 +19,7 @@ require("jquery")
 // const imagePath = (name) => images(name, true)
 $(document).on('turbolinks:load', function() {
   $('.ui.dropdown').dropdown();
+  $('.message .close').on('click', function() {
+    $(this).closest('.message').transition('fade');
+  });
 })
